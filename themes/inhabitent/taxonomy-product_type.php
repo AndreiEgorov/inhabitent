52fa85675dd1d552275c95c56df94042b27115ab
Andrei Egorov <andreyegorov1992@gmail.com>
2018-05-27 16:49:56 -0700

    Add styling to taxonomy-product_type

--- themes/inhabitent/taxonomy-product_type.php
+++ themes/inhabitent/taxonomy-product_type.php
@@ -10,59 +10,58 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
-
-			<div class="item-grid-small">	
-				<?php while ( have_posts() ) : the_post(); ?>
 		
+
+			<div class="content-container">
+
+		
+					<header class="page-header">
+						<?php
+							the_archive_title( '<h1 class="page-title">', '</h1>' );
+							the_archive_description( '<div class="taxonomy-description">', '</div>' );
+						?>
+					</header>
+			
+
+	
+
+				<?php if ( have_posts() ) : ?>
+					
 				
-					<header class="entry-header">
 
-							<div class="item-image">
-								<a href="<?php the_permalink()?>"><?php the_post_thumbnail(); ?></a>
-							</div>
-
-							<div class= "item-info">	
-								<h2 class="item-name"><?php the_title();?></h2> <h2 class="item-price"> <?php echo CFS()->get( 'price' ); ?> </h2>
-							</div>	
+					<ul class="item-grid-small">	
+						<?php while ( have_posts() ) : the_post(); ?>
+				
 						
-					</header><!-- .entry-header -->
-				
+							<li class="shop-item">
 
-				<?php endwhile; ?>
+									<div class="item-image">
+										<a href="<?php the_permalink()?>"><?php the_post_thumbnail(); ?></a>
+									</div>
 
+									<div class= "item-info">	
+										<h2 class="item-name"><?php the_title();?></h2> <span>..............................................................</span> <h2 class="item-price"> <?php echo CFS()->get( 'price' ) ; ?> </h2>
+
+									</div>	
+								
+							</li>
+						
+
+						<?php endwhile; ?>
+
+					</ul>	
+
+
+				<?php else : ?>
+
+					<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+				<?php endif; ?>
 			</div>	
 
 
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
-
+				
+		
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
